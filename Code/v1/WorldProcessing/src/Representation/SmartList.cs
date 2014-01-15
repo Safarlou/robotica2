f168d9319b76cb4fb18ce8581baa37e9350cefd6
KoenDercksen <koen_37@msn.com>
2014-01-15 11:46:34 +0100

    Ready for namespace renaming... Buckle up

---
+++ Code/v1/WorldProcessing/src/Representation/SmartList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldProcessing.Representation
+{
+	public class SmartList
+	{
+		private List<Tuple<Object, int>> Data;
+
+		public SmartList()
+		{
+			this.Data = new List<Tuple<Object, int>>();
+		}
+	}
+}
