f97985325983630ca997cd31ee121b6636e35717
Marein Konings <mail@marein.org>
2014-01-30 15:56:54 +0100

    More images

--- Code/v1/Main/App.xaml.cs
+++ Code/v1/Main/App.xaml.cs
@@ -24,6 +24,28 @@ namespace WorldProcessing
 
 		public App()
 		{
+			var x = new Emgu.CV.Seq<System.Drawing.Point>(new Emgu.CV.MemStorage());
+
+			x.Push(new System.Drawing.Point(1, 0));
+			x.Push(new System.Drawing.Point(0, 5));
+			x.Push(new System.Drawing.Point(2, 10));
+			x.Push(new System.Drawing.Point(7, 11));
+			x.Push(new System.Drawing.Point(8, 15));
+			x.Push(new System.Drawing.Point(12, 13));
+			x.Push(new System.Drawing.Point(9, 6));
+			x.Push(new System.Drawing.Point(4, 5));
+
+			var y = x.GetMinAreaRect();
+
+			foreach (var v in y.GetVertices())
+				Console.WriteLine(v);
+
+			var z = new NavPolygon(y.GetVertices());
+
+			Console.WriteLine(z.Area);
+
+			return;
+
 			//NXTControllers instantiëren
 			transport = new NXTController(null, "CONVOI Transport");
 			guard = new NXTController(null, "CONVOI Guard");
